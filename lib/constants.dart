23de0b9899c39ceda151f9b447007e3a0e2434e0
kmchen <webologix@gmail.com>
2026-06-25 18:03:33 +0200

    3.1.5

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,5 @@ library constants;
 
 const bool debug = true;
 const String appName = "Scrabble P2P";
-const String version = "3.1.4";
+const String version = "3.1.5";
 const defaultRelayServer = "https://relay-server-eu.onrender.com";
